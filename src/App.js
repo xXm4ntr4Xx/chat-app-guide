da7b39927c63d3b843709bd9fda6f4ac2b792eee
Pietro Annobil <pietroannobil@Pietros-MacBook-Air.local>
2022-03-24 14:42:51 +0000

    live-chat

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+import './App.css';
+import React from 'react';
+import { ChatEngine } from 'react-chat-engine';
+
+export function App() {
+	return (
+
+    <>
+		<ChatEngine
+			height='100vh'
+			userName='Pietro'
+			userSecret='300488'
+			projectID='86c552c5-4ac8-48ce-833d-110b19653591'
+		/>
+
+    </>
+	);
 }
 
 export default App;
