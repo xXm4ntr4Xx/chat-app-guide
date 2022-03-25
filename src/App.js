1f7e4045b6ba2416e3ef97f3ca14227736956134
Pietro Annobil <pietroannobil@Pietros-MacBook-Air.local>
2022-03-25 12:14:07 +0000

    dm chat example

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 
 import './App.css';
 import React from 'react';
-import { ChatEngine } from 'react-chat-engine';
-import  ChatFeed  from './ChatFeed.js'; 
+// import { ChatEngine } from 'react-chat-engine';
+import DirectChatPage from './components/DirectChatPage';
+
 
 export function App() {
 	return (
 
     <>
-		
-    <ChatEngine
+	<DirectChatPage/>
+    {/* <ChatEngine
 
 			height='100vh'
 			userName='Pietro'
 			userSecret='300488'
 			projectID='777a7252-fd29-4f52-88d8-756a06d71449'
-      renderChatFeed={chatAppState => <ChatFeed {...chatAppState}/>}
-		/>
-
+		/> */}
     </>
 	);
 }
---
+++ src/components/DirectChatPage.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react'
+
+import { ChatEngine, getOrCreateChat } from 'react-chat-engine'
+
+const DirectChatPage = () => {
+	const [username, setUsername] = useState('')
+
+	function createDirectChat(creds) {
+		getOrCreateChat(
+			creds,
+			{ is_direct_chat: true, usernames: [username] },
+			() => setUsername('')
+		)
+	}
+
+	function renderChatForm(creds) {
+		return (
+			<div>
+				<input 
+					placeholder='Username' 
+					value={username} 
+					onChange={(e) => setUsername(e.target.value)} 
+				/>
+				<button onClick={() => createDirectChat(creds)}>
+					Create
+				</button>
+			</div>
+		)
+	}
+
+	return (
+		<ChatEngine
+			height='100vh'
+			userName='Pietro'
+			userSecret='300488'
+			projectID='777a7252-fd29-4f52-88d8-756a06d71449'
+			renderNewChatForm={(creds) => renderChatForm(creds)}
+		/>
+	)
+}
+
+export default DirectChatPage;
